903189c658183e1147a7952749edf7874b773998
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-20 09:21:35 +0300

    Get away from master-slave terminology

--- warningsystem/settings/base.py
+++ warningsystem/settings/base.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'slaves.apps.SlavesConfig',
+    'subordinates.apps.SubordinatesConfig',
     'sms.apps.SmsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- warningsystem/urls.py
+++ warningsystem/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('slaves/', include('slaves.urls')),
+    path('subordinates/', include('subordinates.urls')),
     path('admin/', admin.site.urls),
 ]
